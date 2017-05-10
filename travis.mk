4922d0fd01bda243711b74393be7bf1ed043989c
François Févotte <francois.fevotte@edf.fr>
2017-05-10 20:44:35 +0200

    Setup continuous integration

---
+++ travis.mk
@@ -0,0 +1,28 @@
+SHELL = /bin/bash
+
+download-valgrind:
+	cd .. && svn checkout "svn://svn.valgrind.org/valgrind/$(VALGRIND_VERSION)" valgrind+verrou >/dev/null
+
+patch-valgrind:
+	cd ../valgrind+verrou && cp -a $(PWD) verrou
+	cd ../valgrind+verrou && patch -p1 <verrou/valgrind.diff
+
+configure:
+	cd ../valgrind+verrou && ./autogen.sh
+	cd ../valgrind+verrou && ./configure --enable-only64bit --prefix=$${PWD}/install
+
+build:
+	cd ../valgrind+verrou && make
+	cd ../valgrind+verrou && make install >/dev/null
+
+check-install:
+	source ../valgrind+verrou/install/env.sh && valgrind --version
+	source ../valgrind+verrou/install/env.sh && valgrind --tool=verrou --help
+
+check:
+	cd ../valgrind+verrou && make -C tests  check
+	cd ../valgrind+verrou && make -C verrou check
+	cd ../valgrind+verrou && perl tests/vg_regtest verrou
+check-error:
+	cd ../valgrind+verrou/verrou/tests && tail -n+1 *.stdout.diff *.stdout.out *.stderr.diff *.stderr.out
+	@false
