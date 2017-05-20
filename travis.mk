eb2ac9442ddded5f15b9c7745f5fc793d565567c
François Févotte <francois.fevotte@edf.fr>
2017-05-21 00:27:52 +0200

    Updated valgrind patch

--- travis.mk
+++ travis.mk
@@ -5,7 +5,7 @@ download-valgrind:
 
 patch-valgrind:
 	cd ../valgrind+verrou && cp -a $(PWD) verrou
-	cd ../valgrind+verrou && patch -p1 <verrou/valgrind.diff
+	cd ../valgrind+verrou && patch -p0 <verrou/valgrind.diff
 
 configure:
 	cd ../valgrind+verrou && ./autogen.sh
