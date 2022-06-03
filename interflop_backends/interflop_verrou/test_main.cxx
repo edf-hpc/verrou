22adc3fa7f72090f88c10cf9408d6c5f76474cf2
BRUNO LATHUILIERE <bruno.lathuiliere@edf.fr>
2022-06-03 17:40:22 +0200

    update test_main for verrou backend

--- interflop_backends/interflop_verrou/test_main.cxx
+++ interflop_backends/interflop_verrou/test_main.cxx
@@ -11,15 +11,15 @@
 void print_debug(int nbArg, const char * op,const  double* a, const double* res){
 
   if(nbArg==1){
-    std::cout << op << " : "<< a[0] << "->"<<&res << std::endl;  
+    std::cout << op << " : "<< a[0] << "->"<<&res << std::endl;
   }
 
   if(nbArg==2){
-    std::cout << op << " : "<< a[0] << "," << a[1]<< "->"<<&res << std::endl;  
+    std::cout << op << " : "<< a[0] << "," << a[1]<< "->"<<&res << std::endl;
   }
 
   if(nbArg==3){
-    std::cout << op << " : "<< a[0] << "," << a[1]<< "," << a[2]<< "->"<<&res << std::endl;  
+    std::cout << op << " : "<< a[0] << "," << a[1]<< "," << a[2]<< "->"<<&res << std::endl;
   }
 
 } ;
@@ -30,27 +30,33 @@ int main(int argc, char** argv){
   void* context;
   struct interflop_backend_interface_t ifverrou=interflop_verrou_init(&context);
 
-  //  interflop_verrou_configure(VR_NEAREST, context);
-  interflop_verrou_configure(VR_FLOAT, context);
+  interflop_verrou_configure(VR_RANDOM_DET, context);
+
+  uint64_t seed=0;
+  seed=1020000002;
+  std::cout << "seed: " << seed<<std::endl;
+  //seed=1000000000;
+  // if(conf.seed==(unsigned int) -1){
+  //   struct timeval t1;
+  //   gettimeofday(&t1, NULL);
+  //   conf.seed =  t1.tv_usec + getpid();
+  // }
+  verrou_set_seed (seed);
 
   verrou_set_debug_print_op(&print_debug);
-  
 
-  double a=0.1;
-  double b=0.1;
-  double c;
-  
-  interflop_verrou_add_double(a,b,&c,context);
 
+  double step=0.1;
+  double acc=0.;
+
+  for(int i=0; i<10000; i++){
+    interflop_verrou_add_double(acc,step,&acc,context);
+  }
   std::cout << std::setprecision(16);
-  std::cout << "c: "<<c << std::endl; 
+  std::cout << "acc: "<<acc << std::endl;
 
+  interflop_verrou_finalize(context);
 
-  
-  interflop_verrou_finalyze(context);
-  
 
   return 0;
 }
-
-
