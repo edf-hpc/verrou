07de6bdfe547c6f705be5c9354bdfccae78682a2
BRUNO LATHUILIERE <bruno.lathuiliere@edf.fr>
2022-07-13 10:59:42 +0200

    stagnation graph switch to log scale

--- interflop_backends/interflop_verrou/examples/stagnation/stagnation.cxx
+++ interflop_backends/interflop_verrou/examples/stagnation/stagnation.cxx
@@ -8,14 +8,17 @@
 #include <iomanip>
 #include <vector>
 #include <string>
+
+
+
 int  main(int argc, char** argv){
   float init=100000;
   float inc=0.1;
 
   std::vector<vr_RoundingMode> roundingMode={VR_NEAREST, VR_RANDOM, VR_RANDOM_DET,VR_AVERAGE, VR_AVERAGE_DET};
 
-  
- 
+
+
   for(size_t i=0; i< roundingMode.size();i++){
     std::string name=verrou_rounding_mode_name(roundingMode[i]);
     std::ofstream outStream(name+std::string(".out"));
@@ -23,20 +26,21 @@ int  main(int argc, char** argv){
     void* context;
     struct interflop_backend_interface_t ifverrou=interflop_verrou_init(&context);
     interflop_verrou_configure(roundingMode[i], context);
-    uint64_t seed(42);    
+    uint64_t seed(42);
     verrou_set_seed (seed);
 
     float acc=init;
     int stagnation=-1;
     int nb_stagnation=0;
+    int nextPower=1;
+    float step=1.1;
     for(int i=0; i<=100000000; i++){
-      if( (i%1000==0 && i<10000)
-	  ||( i%10000==0 && i <1000000)
-	  ||( i%100000==0) 
-	  ){
+      if( i==nextPower){
 	double ref= init + ((double)i)*inc;
 	double accError= (double) acc- ref;
 	outStream << i<<"\t"<<acc << "\t"<< accError<<"\t" << std::abs(accError/ref)<< std::endl;
+	nextPower= (int)(nextPower*step);
+	if(i==nextPower) nextPower=i+1;
       }
       float acc_new;
       interflop_verrou_add_float(acc,inc,&acc_new,context);
@@ -58,5 +62,5 @@ int  main(int argc, char** argv){
       std::ofstream outStaStream(name+std::string(".STAGNATION.out"));
       outStaStream <<stagnation <<std::endl;
     }
-  } 
-}  
+  }
+}
--- interflop_backends/interflop_verrou/examples/stagnation/stagnationLog.gp
+++ interflop_backends/interflop_verrou/examples/stagnation/stagnationLog.gp
@@ -1,18 +1,17 @@
 
 set terminal pdf
 
-set output "stagnationError.pdf"
-unset logscale x
+set output "stagnationErrorLog.pdf"
+set logscale x
 set logscale y
 set yrange [1e-7:1]
-set xrange [0:2e7]
+set xrange [1000:1.e8]
 set ylabel "relative error"
-set xlabel "summation index i"
-#set format y "%.0s*10^{%T}"
-set format y "%2.0t{/Symbol \264}10^{%L}"
+set xlabel "increment number"
+
 set format y "10^{%L}"
-set key bottom center
-set key at 14000000, 0.0000004
+set key top left
+#set key at 14000000, 0.0000004
 set grid
 
 stagnationAverageDet=`cat AVERAGE_DET.STAGNATION.out`
@@ -21,9 +20,9 @@ stagnationNearest=`cat NEAREST.STAGNATION.out`
 
 
 
-set arrow from stagnationAverageDet, graph 0 to stagnationAverageDet, graph 1 nohead  linecolor 0x008B8B
-set arrow from stagnationRandomDet, graph 0 to stagnationRandomDet, graph 1 nohead  linecolor "orange-red"
-set arrow from stagnationNearest, graph 0 to stagnationNearest, graph 1 nohead  linecolor "dark-green"
+set arrow from stagnationAverageDet, graph 0 to stagnationAverageDet, graph 1 nohead  linecolor 0x008B8B dashtype 5 lw 2
+set arrow from stagnationRandomDet, graph 0 to stagnationRandomDet, graph 1 nohead  linecolor "orange-red" dashtype 2 lw 2
+set arrow from stagnationNearest, graph 0 to stagnationNearest, graph 1 nohead  linecolor "dark-green" dashtype 4 lw 2
 
 plot "NEAREST.out"     using 1:4 title 'nearest' lc "dark-green" pt 3	,\
      "RANDOM.out"     using 1:4 title 'random' lc "dark-red" pt 1 ,\
