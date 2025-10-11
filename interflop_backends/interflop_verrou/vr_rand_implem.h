7dba023b23fdab979dc9325044ca52de96879575
Bruno LATHUILIERE <lathuiliere-github@tuxyl.net>
2025-10-12 00:18:30 +0200

    add 16,32,64 value for VERROU_NUM_AVG compil option

--- interflop_backends/interflop_verrou/vr_rand_implem.h
+++ interflop_backends/interflop_verrou/vr_rand_implem.h
@@ -159,26 +159,43 @@ inline bool vr_rand_bool (Vr_Rand * r) {
 //#define VERROU_NUM_AVG 2
 //#endif
 
-#if VERROU_NUM_AVG==8
-constexpr uint64_t maskAvg = 0x00000000000000FF;  ;
-constexpr uint64_t shiftAvgTab[]= {0, 8 , 16, 24, 32, 40, 48, 56 };
-constexpr uint32_t loopAvg = 8  ;
-constexpr double maxAvgInv(1./256.);
-#elif VERROU_NUM_AVG==4
-constexpr uint64_t maskAvg = 0x000000000000FFFF;  ;
-constexpr uint64_t shiftAvgTab[]= {0,16, 32, 48};
-constexpr uint32_t loopAvg = 4  ;
-constexpr double maxAvgInv(1/65536.);
+#if VERROU_NUM_AVG==2
+constexpr uint64_t maskAvg = 0x00000000FFFFFFFF;  ;
+constexpr uint64_t shiftAvgTab[]={0,32} ;
+constexpr uint32_t loopAvg = 2  ;
+constexpr double maxAvgInv(1/4294967296.);
 #elif VERROU_NUM_AVG==3
 constexpr uint64_t maskAvg = 0x00000000001FFFFF;// 21bit
 constexpr uint64_t shiftAvgTab[]={0,21,42} ;
 constexpr uint32_t loopAvg = 3  ;
 constexpr double maxAvgInv(1/2097152.);
-#elif VERROU_NUM_AVG==2
-constexpr uint64_t maskAvg = 0x00000000FFFFFFFF;  ;
-constexpr uint64_t shiftAvgTab[]={0,32} ;
-constexpr uint32_t loopAvg = 2  ;
-constexpr double maxAvgInv(1/4294967296.);
+#elif VERROU_NUM_AVG==4
+constexpr uint64_t maskAvg = 0x000000000000FFFF;  ;
+constexpr uint64_t shiftAvgTab[]= {0,16, 32, 48};
+constexpr uint32_t loopAvg = 4  ;
+constexpr double maxAvgInv(1/65536.);
+#elif VERROU_NUM_AVG==8
+constexpr uint64_t maskAvg = 0x00000000000000FF;  ;
+constexpr uint64_t shiftAvgTab[]= {0, 8 , 16, 24, 32, 40, 48, 56 };
+constexpr uint32_t loopAvg = 8  ;
+constexpr double maxAvgInv(1./256.);
+#elif VERROU_NUM_AVG==16
+constexpr uint64_t maskAvg = 0x000000000000000F;  ;
+constexpr uint64_t shiftAvgTab[]= {0,4,8,12,16,20,24,28,32,36,40,44,48,52,56,60};
+constexpr uint32_t loopAvg = 16  ;
+constexpr double maxAvgInv(.25);
+#elif VERROU_NUM_AVG==32
+constexpr uint64_t maskAvg = 0x0000000000000003;  ;
+constexpr uint64_t shiftAvgTab[]= {0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,38,40,42,44,46,48,50,52,54,56,58,60,62};
+constexpr uint32_t loopAvg = 32  ;
+constexpr double maxAvgInv(.5);
+#elif VERROU_NUM_AVG==64
+//only for test purpose (should be equivalent to random)
+constexpr uint64_t maskAvg = 0x0000000000000001;  ;
+constexpr uint64_t shiftAvgTab[]= {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63};
+constexpr uint32_t loopAvg = 64  ;
+constexpr double maxAvgInv(1.);
+
 #elif VERROU_NUM_AVG==1
 #else
 #error 'VERROU_NUM_AVG is not defined'
