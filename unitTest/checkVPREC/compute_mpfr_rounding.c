c79aba0966eee2b5523389f541f4aa86ad09437f
Bruno LATHUILIERE <lathuiliere-github@tuxyl.net>
2026-02-01 15:54:59 +0100

    compute_mpfr_rounding: fix mpfr emin + optional debug help (comparison to float32)

--- unitTest/checkVPREC/compute_mpfr_rounding.c
+++ unitTest/checkVPREC/compute_mpfr_rounding.c
@@ -39,14 +39,14 @@ void strLineToDoubleTab(double* tab, int nbArgs,  char *string) {
 }
 
 
-mpfr_exp_t get_emax(int range) {
+mpfr_exp_t get_emax_for_mpfr(int range) {
   int emax = 1 << (range - 1);
   return emax;
 }
 
-mpfr_exp_t get_emin(int range, int precision) {
+mpfr_exp_t get_emin_for_mpfr(int range, int precision) {
   int emin = 1 << (range - 1);
-  emin = -emin - precision + 4; //why?
+  emin = -emin - precision + 3; // https://stackoverflow.com/questions/38664778/subnormal-numbers-in-different-precisions-with-mpfr
   return emin;
 }
 
@@ -67,6 +67,25 @@ void get_smallest_positive_subnormal_number(mpfr_t smallest_subnormal, int range
   mpfr_set_emin(_emin);
   mpfr_set_emax(_emax);
 }
+
+void get_smallest_normal_number(mpfr_t smallest_normal, int range) {
+  mpfr_exp_t _emin = mpfr_get_emin();
+  mpfr_exp_t _emax = mpfr_get_emax();
+
+  int emax = (1 << (range - 1)) - 1;
+  int emin = 1 - emax;
+
+  mpfr_set_emin(mpfr_get_emin_min());
+  mpfr_set_emax(mpfr_get_emax_max());
+
+  mpfr_init2(smallest_normal, 256);
+  /* 2 ^ (emin - precision)*/
+  mpfr_set_ui_2exp(smallest_normal, 1, emin, MPFR_RNDN);
+
+  mpfr_set_emin(_emin);
+  mpfr_set_emax(_emax);
+}
+
 
 void get_largest_positive_normal_number(mpfr_t largest_normal, int range, int precision) {
 
@@ -153,33 +172,100 @@ void apply_operation(mpfr_t res, mpfr_t* args, const char op,
    mpfr_subnormalize(res, i, MPFR_RNDN);
 }
 
+//#define DEBUG_CMP_FLOAT
+#ifdef DEBUG_CMP_FLOAT
+#include <assert.h>
+#endif
+
 void vprec_rounding(mpfr_t x, int range, int precision) {
-  mpfr_clear_flags();
-  mpfr_exp_t emax = get_emax(range);
-  mpfr_exp_t emin = get_emin(range,precision);
+#ifdef DEBUG_CMP_FLOAT
+   assert(range==8);
+   assert(precision==23);
+   double d = mpfr_get_d(x, MPFR_RNDN);
+   float fRef = mpfr_get_flt(x, MPFR_RNDN);
+   float df=(float)d;
+   assert(fRef==df);
+#endif
+   mpfr_clear_flags();
+  mpfr_exp_t emax = get_emax_for_mpfr(range);
+  mpfr_exp_t emin = get_emin_for_mpfr(range, precision);
 
-  mpfr_set_emax(emax);
-  mpfr_set_emin(emin);
-  int i = mpfr_prec_round(x, precision+1, MPFR_RNDN);
-
-  i = mpfr_check_range(x, i, MPFR_RNDN);
-
-  mpfr_t smallest_subnormal, largest_normal;
-  get_smallest_positive_subnormal_number(smallest_subnormal, range, precision);
-  get_largest_positive_normal_number(largest_normal, range, precision);
-
-  if (mpfr_cmpabs(x, largest_normal) > 0) {
-    if (verbose_mode)
-      fprintf(stderr, "Overflow detected\n");
-    mpfr_set_inf(x, mpfr_sgn(x));
-  } else if (mpfr_cmpabs(x, smallest_subnormal) < 0) {
-    if (verbose_mode)
-      fprintf(stderr, "Underflow detected\n");
-    mpfr_set_zero(x, mpfr_sgn(x));
+  if( verbose_mode){
+     printf("emax %ld\t emin %ld\n", emax, emin);
   }
 
-  i = mpfr_subnormalize(x, i, MPFR_RNDN);
+  mpfr_t xRound;
+  mpfr_set_emax(emax);
+  mpfr_set_emin(emin);
+  mpfr_inits2(precision+1,xRound, (mpfr_ptr)0);
+  int i=mpfr_set(xRound, x,MPFR_RNDN);
+  i = mpfr_check_range(xRound, i, MPFR_RNDN);
+  i = mpfr_subnormalize(xRound, i, MPFR_RNDN);
+
+  mpfr_t smallest_subnormal, smallest_normal,largest_normal;
+  get_smallest_positive_subnormal_number(smallest_subnormal, range, precision);
+  get_largest_positive_normal_number(largest_normal, range, precision);
+  get_smallest_normal_number(smallest_normal,range);
+
+  if (mpfr_cmpabs(xRound, largest_normal) > 0) {
+    if (verbose_mode)
+      fprintf(stderr, "Overflow detected\n");
+    mpfr_set_inf(xRound, mpfr_sgn(x));
+  }else{
+     if (mpfr_cmpabs(xRound, smallest_normal)<0){
+        if (verbose_mode){
+           fprintf(stderr, "Subnormal detected limit(%e)\n",  mpfr_get_d(smallest_normal,MPFR_RNDN));
+        }
+
+        if (mpfr_cmpabs(xRound, smallest_subnormal) < 0) {
+           if (verbose_mode)
+              fprintf(stderr, "Underflow detected %e\n", mpfr_get_d(smallest_subnormal, MPFR_RNDN));
+           mpfr_set_zero(xRound, mpfr_sgn(x));
+        }
+     }
+  }
+
+  mpfr_set(x, xRound,MPFR_RNDN);
+
+#ifdef DEBUG_CMP_FLOAT
+  double dRes = mpfr_get_d(xRound, MPFR_RNDN);
+  if(dRes!=df && (! (dRes!=dRes && df!=df ) ) ){
+     printf("d:%.17e  dRes: %.9e  float: %.9e\n", d, dRes, df);
+     printf("d:%a  dRes: %a  float: %a\n", d, dRes, df);
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  ((byte) & 0x80 ? '1' : '0'), \
+  ((byte) & 0x40 ? '1' : '0'), \
+  ((byte) & 0x20 ? '1' : '0'), \
+  ((byte) & 0x10 ? '1' : '0'), \
+  ((byte) & 0x08 ? '1' : '0'), \
+  ((byte) & 0x04 ? '1' : '0'), \
+  ((byte) & 0x02 ? '1' : '0'), \
+  ((byte) & 0x01 ? '1' : '0')
+
+     float fRes=(float)dRes;
+     float ff=(float)df;
+
+     if(ff==fRes){
+        printf("Problem: sortie MPFR non representable en float\n");
+     }
+     int32_t resToPrint=*(int32_t*)(&fRes);
+     int32_t fToPrint=  *(int32_t*)(&ff);
+     printf("dRes: "BYTE_TO_BINARY_PATTERN" "BYTE_TO_BINARY_PATTERN" "BYTE_TO_BINARY_PATTERN" "BYTE_TO_BINARY_PATTERN"\n",
+            BYTE_TO_BINARY(resToPrint>>24) ,
+            BYTE_TO_BINARY(resToPrint >>16),
+            BYTE_TO_BINARY(resToPrint>>8),
+            BYTE_TO_BINARY(resToPrint ));
+     printf("ff  : "BYTE_TO_BINARY_PATTERN" "BYTE_TO_BINARY_PATTERN" "BYTE_TO_BINARY_PATTERN" "BYTE_TO_BINARY_PATTERN"\n",
+            BYTE_TO_BINARY(fToPrint>>24) ,
+            BYTE_TO_BINARY(fToPrint >>16),
+            BYTE_TO_BINARY(fToPrint>>8),
+            BYTE_TO_BINARY(fToPrint ));
+  };
+#endif
 }
+
 
 
 void generate_ref(double* arg, char op, fptype_t type, FILE* fp,
@@ -236,12 +322,20 @@ int main(int argc, char *argv[]) {
 
   fptype_t fpTypeTab[]={float_type, double_type};
   char*  fpTypeTabStr[]={"float", "double"};
+
+#ifdef DEBUG_CMP_FLOAT
+  int fpRangeMin[]={-1, 8};
+  int fpRangeMax[]={-1, 8};
+
+  int fpPrecisionMin[]={-1, 23};
+  int fpPrecisionMax[]={-1, 23};
+#else
   int fpRangeMin[]={2, 2};
   int fpRangeMax[]={8, 11};
 
   int fpPrecisionMin[]={2, 2};
   int fpPrecisionMax[]={23, 52};
-
+#endif
   char opTabStr[]={'+','-','x','/', 's', 'f'}; //s for sqrt and f for fma
 
   vprec_mode_t modeTab[]={pb, ob, full};
@@ -251,7 +345,11 @@ int main(int argc, char *argv[]) {
      vprec_mode_t vprec_mode=modeTab[indexMode];
      printf("Generate %s reference\n", modeTabStr[indexMode]);
 
+#ifdef DEBUG_CMP_FLOAT
+     for(int indexType=1; indexType <2; indexType++){
+#else
      for(int indexType=0; indexType <2; indexType++){
+#endif
         fptype_t fpType=fpTypeTab[indexType];
         printf("Generate %s reference\n", fpTypeTabStr[indexType]);
 
